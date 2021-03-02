461ccb104cde919ffb08976bb066442bed5a6354
Hannah Armstrong <hannah.armstrong1901@gmail.com>
2021-03-02 11:40:26 -0800

    Wrote third test, tests fizzbuzz / multiple of 15.

--- test1.py
+++ test1.py
@@ -5,3 +5,4 @@ class TestProgram(unittest.TestCase):
     def test_fizzbuzz(self):
         self.assertEqual(part1.fizz(3),("Fizz"))
         self.assertEqual(part1.buzz(5),("Buzz"))
+        self.assertEqual(part1.fizzbuzz(15),("FizzBuzz"))
