dd1bab51d4681e28ef5c955c4e3e2e59c67c18e4
Hannah Armstrong <hannah.armstrong1901@gmail.com>
2021-03-02 11:35:43 -0800

    Implemented fizz method.

---
+++ part1.py
@@ -0,0 +1,5 @@
+def fizz(x):
+    if(x % 3 == 0):
+        return "Fizz"
+    else:
+        return str(x)
--- test1.py
+++ test1.py
@@ -1,1 +1,6 @@
 import unittest
+import part1
+
+class TestProgram(unittest.TestCase):
+    def test_fizzbuzz(self):
+        self.assertEqual(part1.fizz(3),("Fizz"))
