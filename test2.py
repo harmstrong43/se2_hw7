16607301d650e850825b5b4abfd1f95565c5b75a
Hannah Armstrong <hannah.armstrong1901@gmail.com>
2021-03-02 12:20:35 -0800

    Added test for years divisble by  400.

--- test2.py
+++ test2.py
@@ -4,4 +4,5 @@ import part2
 class TestProgram(unittest.TestCase):
     def test_leapyear(self):
         self.assertEqual(part2.leap(4),True)
-        self.asertEqual(part2.leap(100),True)
+        self.assertEqual(part2.leap(100),True)
+        self.assertEqual(part2.leap(400),False)
